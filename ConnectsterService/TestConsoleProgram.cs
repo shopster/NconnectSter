360dc3d1cd51ffa1045c9bc4e798a8fb7ff72d00
westaj <westaj@e5networks.com>
2010-05-26 12:13:03 -0600

    Added test console program to service project

---
+++ ConnectsterService/TestConsoleProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Connectster.Server;
+
+namespace ConnectsterService
+{
+    static class TestConsoleProgram
+    {
+        static void Main()
+        {
+            var server = new ConnectsterServer();
+            server.Start();
+        }
+    }
+}
