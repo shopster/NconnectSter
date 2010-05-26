9c87bc4fa88902f3018cb86c3a2918e9eabb2c2d
westaj <westaj@e5networks.com>
2010-05-26 17:28:17 -0600

    Enabled service

--- ConnectsterService/Service.cs
+++ ConnectsterService/Service.cs
@@ -1,25 +1,33 @@
 ﻿using System.ServiceProcess;
+using System.Threading;
 using Connectster.Server;
 
 namespace ConnectsterService
 {
     partial class Service : ServiceBase
     {
+        private Thread _thread;
+
         private readonly ConnectsterServer _server;
         public Service()
         {
             InitializeComponent();
             _server = new ConnectsterServer();
+            var job = new ThreadStart(_server.Start);
+            _thread = new Thread(job);
         }
 
         protected override void OnStart(string[] args)
         {
-            _server.Start(); 
+            _thread.Start();
         }
 
         protected override void OnStop()
         {
             _server.Stop();
+            const int threeMinutes = 1000*60*60*3;
+            Thread.Sleep(threeMinutes);
+            _thread.Abort();
         }
     }
 }
---
+++ ConnectsterService/ServiceInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System;using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+
+
+namespace ConnectsterService
+{
+    [RunInstaller(true)]
+    public partial class ServiceInstaller : Installer
+    {
+        public ServiceInstaller()
+        {
+            InitializeComponent();
+
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "Connectster Server";
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
