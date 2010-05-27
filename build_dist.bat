12585b6e91f6d48a90700771b1fb2a4f62eb7aed
westaj <westaj@e5networks.com>
2010-05-27 10:34:12 -0600

    Added db to dist script

--- build_dist.bat
+++ build_dist.bat
@@ -2,6 +2,6 @@
 c:\windows\microsoft.net\framework\v3.5\msbuild.exe /p:Configuration=Release
 rmdir /s /q dist
 mkdir dist
-mkdir dist\log
 xcopy /s ConnectsterAuthentication\*.* dist\ConnectsterAuthentication\
 xcopy /s ConnectsterService\bin\release\*.* dist\ConnectsterService\
+xcopy /s db\*.* dist\db\
